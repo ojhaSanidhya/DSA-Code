10a176dacbab88b872b65cd53c80cc929905d19e
ojhaSanidhya <132838074+ojhaSanidhya@users.noreply.github.com>
2024-08-05 23:47:08 +0530

    Time: 115 ms (16.32%) | Memory: 130.9 MB (26.58%) - LeetSync

---
+++ 134-gas-station/gas-station.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
+        int balance = 0;
+        int deficit = 0;
+        int start = 0;
+        for(int i = 0; i<gas.size(); i++){
+            balance = balance + gas[i] - cost[i];
+            if(balance < 0){
+                deficit = deficit + abs(balance);
+                balance = 0;
+                start = i+1;
+            }
+        }
+
+        if(balance - deficit >= 0){
+            return start;
+        }
+        else{
+            return -1;
+        }
+    }
+};
