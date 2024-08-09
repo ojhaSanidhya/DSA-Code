250bc4b5f62fe8c65ab56f37647271eeb57c6c28
ojhaSanidhya <132838074+ojhaSanidhya@users.noreply.github.com>
2024-08-09 19:33:40 +0530

    Time: 8 ms (37.38%) | Memory: 18 MB (22.70%) - LeetSync

--- 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.cpp
+++ 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.cpp
@@ -11,15 +11,54 @@
  */
 class Solution {
 public:
-    int maxDepth(TreeNode* root) {
-    //base case
-	if(root == NULL ) {
-	    return 0;
-	}
+int levelOrderTraversal(TreeNode* root){
+    queue<TreeNode*>q;
+    q.push(root);
+    q.push(NULL);
+    int count = 1;
 
-	int leftHeight = maxDepth(root->left);
-	int rightHeight = maxDepth(root->right);
-	int height = max(leftHeight, rightHeight) + 1;
-	return height;
+    if(root == NULL){
+        return 0;
+    }
+
+    //asli traversal start krete h 
+
+    while(!q.empty()){
+        if(q.front() == NULL){
+            cout<<endl;
+            q.pop();
+            if(!q.empty()){
+                q.push(NULL);
+                count++;
+            }
+        }
+        else{
+            //valid node wala case
+            TreeNode* temp = q.front();
+            q.pop();
+            cout<<temp->val<<" ";
+
+            if(temp->left != NULL){
+                q.push(temp->left);
+            }
+            if(temp->right != NULL){
+                q.push(temp->right);
+            }
+        }
+    }
+    return count;
+}
+    int maxDepth(TreeNode* root) {
+        int count = levelOrderTraversal(root);
+        return count;
+    //base case
+	// if(root == NULL ) {
+	//     return 0;
+	// }
+
+	// int leftHeight = maxDepth(root->left);
+	// int rightHeight = maxDepth(root->right);
+	// int height = max(leftHeight, rightHeight) + 1;
+	// return height;
     }
 };
