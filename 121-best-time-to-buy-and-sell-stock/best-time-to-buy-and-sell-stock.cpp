354605255ffe5c4a72c146413f27ddc4fabdcc60
ojhaSanidhya <132838074+ojhaSanidhya@users.noreply.github.com>
2024-08-15 19:32:33 +0530

    Time: 92 ms (29.99%) | Memory: 96 MB (61.92%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
@@ -51,6 +51,14 @@ public:
         // else{
         //     return 0;
         // }
-        
+
+        int mini = prices[0];
+        int profit = 0;
+        for(int i = 1; i<prices.size(); i++){
+            int cost = prices[i] - mini;
+            profit = max(profit,cost);
+            mini = min(mini,prices[i]);
+        }
+       return profit; 
     }
 };
